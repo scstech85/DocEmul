22678d6c746729a30e528092d6291edd9677f860
scstech <samuele.capo@gmail.com>
2017-11-11 11:56:24 +0100

    update brandenburg

---
+++ Data/Brandenburg/download_extract_background_files.sh
@@ -0,0 +1,4 @@
+
+wget -O Data/Brandenburg/Background.zip "https://www.dropbox.com/s/90f6tqrf180ahi9/Background.zip?dl=0"
+unzip Data/Brandenburg/Background.zip -d Data/Brandenburg/
+rm -f Data/Brandenburg/Background.zip
