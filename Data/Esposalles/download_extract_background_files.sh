3bc95a8bbfe2851ead526992144d41b19afa5a2a
scstech <samuele.capo@gmail.com>
2017-11-11 11:45:51 +0100

    update esposalles

---
+++ Data/Esposalles/download_extract_background_files.sh
@@ -0,0 +1,4 @@
+
+wget -O Data/Esposalles/Background.zip "https://www.dropbox.com/s/k4nphdv924oloo7/Background.zip?dl=0"
+unzip Data/Esposalles/Background.zip -d Data/Esposalles/
+rm -f Data/Esposalles/Background.zip
