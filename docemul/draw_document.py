e27c0769bd572592310625f7e8c6876733345d98
scstech <samuele.capo@gmail.com>
2017-11-13 04:21:09 +0900

    update brandenburg

--- docemul/draw_document.py
+++ docemul/draw_document.py
@@ -304,7 +304,7 @@ class DrawDocument:
 
                 text_height = int(cell.text_height / float(point_pixel) * 12)
 
-                #fonts_path = 'handwritten', text_font = "FountainPen.ttf"
+                #fonts_path = 'handwritten2', text_font = "FountainPen.ttf"
 
                 font_path = self.fonts[cell.type_font].path
 
